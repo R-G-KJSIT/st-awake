76c38454d10e8a4b18d61c8150086cb3afa8a648
R <110241863+R-G-KJSIT@users.noreply.github.com>
2024-10-07 10:10:26 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,6 +3,7 @@
 STREAMLIT_APPS = [
     "https://knreport.streamlit.app/",
     "https://ll370-rs.streamlit.app",
+    "https://r-skill-id.streamlit.app/",
     "https://oam-querystream.streamlit.app",
     "https://oam-automl.streamlit.app"
 ]
